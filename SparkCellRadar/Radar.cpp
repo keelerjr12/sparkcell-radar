54c94437764f5c46097ed02dc9e02b40c8d54ad6
Joshua Keeler <keelerjr12@gmail.com>
2023-01-27 21:49:55 -0600

    bearing, range, and ATA working correctly

--- SparkCellRadar/Radar.cpp
+++ SparkCellRadar/Radar.cpp
@@ -36,7 +36,19 @@ Radar::LLA getTelemetryData(const P3D::IBaseObjectV400& obj) {
 	return Radar::LLA(latitude, longitude, altitude);
 }
 
-SparkCell::RadarHit computeRadarHit(const Radar::LLA& lla_1, const Radar::LLA& lla_2) {
+Radar::PBH getPBH(const P3D::IBaseObjectV400& obj) {
+	auto pitch = 0.0;
+	obj.GetProperty(L"PLANE PITCH DEGREES", L"Degrees", pitch);
+	auto bank = 0.0;
+	obj.GetProperty(L"PLANE BANK DEGREES", L"Degrees", bank);
+	auto heading = 0.0;
+	obj.GetProperty(L"PLANE HEADING DEGREES TRUE", L"Degrees", heading);
+
+	return Radar::PBH{ pitch, bank, heading };
+
+}
+
+float calculateDistance(const Radar::LLA& lla_1, const Radar::LLA& lla_2) {
 	const auto phi_1 = lla_1.Lat * (std::numbers::pi / 180);
 	const auto phi_2 = lla_2.Lat * (std::numbers::pi / 180);
 	const auto phi_delta = (lla_2.Lat - lla_1.Lat) * (std::numbers::pi / 180);
@@ -46,18 +58,42 @@ SparkCell::RadarHit computeRadarHit(const Radar::LLA& lla_1, const Radar::LLA& l
 	const auto c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
 	const auto R = 3440.1;
-	const float d = R * c;
+	const float dist = R * c;
 
-	const auto y = sin(lla_2.Lon - lla_1.Lon) * cos(lla_2.Lat);
-	const auto x = (cos(lla_1.Lat) * sin(lla_2.Lat)) - (sin(lla_1.Lat) * cos(lla_2.Lat) * cos(lla_2.Lon - lla_1.Lon));
-	const auto theta = atan2(y, x);
-	const auto brng = ((theta * 180 / std::numbers::pi) + 360) % 360;
+	return dist;
+}
 
-	return SparkCell::RadarHit{ brng, d };
+float radians(float degs) {
+	return degs * std::numbers::pi / 180;
+}
+
+float degrees(float rads) {
+	return rads * 180 / std::numbers::pi;
 }
 
 float calculateBearing(const Radar::LLA& lla_1, const Radar::LLA& lla_2) {
-	
+	const auto theta_1 = radians(lla_1.Lat);
+	const auto theta_2 = radians(lla_2.Lat);
+	const auto delta_1 = radians(lla_2.Lat - lla_1.Lat);
+	const auto delta_2 = radians(lla_2.Lon - lla_1.Lon);
+
+	const auto y = sin(delta_2) * cos(theta_2);
+	const auto x = cos(theta_1) * sin(theta_2) - sin(theta_1) * cos(theta_2) * cos(delta_2);
+	auto brng = atan2(y, x);
+
+	brng = degrees(brng);
+	brng = static_cast<int>(brng + 360) % 360;
+
+	return brng;
+}
+
+float calculateAON(const Radar::LLA& userLLA, const Radar::PBH& userPBH, const Radar::LLA& rhLLA) {
+	auto aon = static_cast<int>(calculateBearing(userLLA, rhLLA) - userPBH.Heading + 360) % 360;
+
+	if (aon > 180)
+		aon = aon - 360;
+
+	return aon;
 }
 
 void SparkCell::Radar::Update() {
@@ -67,7 +103,9 @@ void SparkCell::Radar::Update() {
 	if (SUCCEEDED(P3D::PdkServices::GetSimObjectManager()->GetUserObject(&spUserObject))) {
 		auto userId = spUserObject->GetId();
 		auto userTelemetry = getTelemetryData(*spUserObject);
-		std::cout << userId << ", " << userTelemetry.Lat << ", " << userTelemetry.Lon << ", " << userTelemetry.Alt << std::endl;
+		auto userPBH = getPBH(*spUserObject);
+
+		std::cout << userId << ", " << userTelemetry.Lat << ", " << userTelemetry.Lon << ", " << userTelemetry.Alt << ", " << userPBH.Heading << std::endl;
 
 		UINT32 nObjects = 100;
 		UINT idArr[100];
@@ -82,8 +120,12 @@ void SparkCell::Radar::Update() {
 				auto radarHitTelemetry = getTelemetryData(*aiObject);
 				std::cout << objId << ", " << radarHitTelemetry.Lat << ", " << radarHitTelemetry.Lon << ", " << radarHitTelemetry.Alt << std::endl;
 
-				const auto rh = computeRadarHit(userTelemetry, radarHitTelemetry);
-				mRadarHits.push_back(rh);
+				const auto brng = calculateBearing(userTelemetry, radarHitTelemetry);
+				const auto aon = calculateAON(userTelemetry, userPBH, radarHitTelemetry);
+				const auto dist = calculateDistance(userTelemetry, radarHitTelemetry);
+
+				std::cout << "brng: " << brng << ", aon: " << aon << "dist: " << dist << std::endl;
+				mRadarHits.push_back({aon, dist});
 			}
 		}
 	}
